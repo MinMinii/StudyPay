a8c49e844624acf024e1c93f3dc60cf069c17982
MinMinii <127048742+MinMinii@users.noreply.github.com>
2025-06-04 12:57:24 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,7 @@
-
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to StudyPay</h1>
+    </main>
+  );
+}
