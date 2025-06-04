4dca3eb1a1a3e010bbe8636c926b14a35d1c016e
MinMinii <127048742+MinMinii@users.noreply.github.com>
2025-06-04 21:25:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  i18n: {
+    locales: ['fa', 'en'],
+    defaultLocale: 'fa',
+    localeDetection: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
