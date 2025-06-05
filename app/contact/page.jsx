7d9ebdea126ec8d95dbd1535e1ea876d563e30a7
MinMinii <127048742+MinMinii@users.noreply.github.com>
2025-06-05 08:16:34 +0200

    Delete app/contact directory

--- app/contact/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function ContactPage() {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-800">
-      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
-      <p>Email us at: <a href="mailto:info@studypay.info" className="text-blue-500">info@studypay.info</a></p>
-    </main>
-  );
-}
